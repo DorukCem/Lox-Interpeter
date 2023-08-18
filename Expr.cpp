2311319fb5e90ca4d94c5afb213e33f56ed1f182
Doruk Cem <doruk.cem@gmail.com>
2023-08-18 19:20:14 +0300

    Expr class finished(?)

--- Expr.cpp
+++ Expr.cpp
@@ -1,2 +1,49 @@
 #include "headers/Expr.h"
 
+// * The visitor has a differet method for each class
+// * When a Visitor "visits" an Expr 
+// * The Expr calls the method that is asssociated with itself and passes a pointer pointing to itself
+// ** -- For example : 
+// ** --    Visitor visits a BinaryExpr by calling its accept method and passing itself by reference
+// ** --    the BinaryExpr calls the visit_BinaryExpr method of the visitor and passes a pointer to itself as an argument  
+
+Binary::Binary(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right)
+   : left(left), op(op), right(right)
+{ }
+
+std::any Binary::accept(ExprVisitor &visitor) 
+{
+   return visitor.visit_BinaryExpr(shared_from_this());
+}
+// *----------------Group----------------------
+Group::Group(std::shared_ptr<Expr> expr_in) 
+   : expr_in(expr_in)
+{ }
+
+std::any Group::accept(ExprVisitor &visitor)
+{
+   return visitor.visit_GroupExpr(shared_from_this());
+}
+
+// *----------------Literal----------------------
+
+Literal::Literal(std::any value) 
+   : value(value)
+{ }
+
+std::any Literal::accept(ExprVisitor &visitor)
+{
+   return visitor.visit_LiteralExpr(shared_from_this());
+}
+
+// *-----------------Unary-----------------------
+
+Unary::Unary(Token op, std::shared_ptr<Expr> right) 
+   : op(op), right(right)
+{ }
+
+std::any Unary::accept(ExprVisitor &visitor)
+{
+   return visitor.visit_UnaryExpr(shared_from_this());
+}
+
--- headers/Expr.h
+++ headers/Expr.h
@@ -1,20 +1,63 @@
 #pragma once
 #include <memory>
-#include "headers/Token.h"
+#include "Token.h"
 
-struct Expr {
-   // put pure virtual function here
+
+struct Binary;
+struct Group;
+struct Literal;
+struct Unary;
+
+struct ExprVisitor {
+  virtual std::any visit_BinaryExpr(std::shared_ptr<Binary> expr) = 0;
+  virtual std::any visit_GroupExpr(std::shared_ptr<Group> expr) = 0;
+  virtual std::any visit_LiteralExpr(std::shared_ptr<Literal> expr) = 0;
+  virtual std::any visit_UnaryExpr(std::shared_ptr<Unary> expr) = 0;
+  virtual ~ExprVisitor() = default;
 };
 
+struct Expr {
+   virtual std::any accept(ExprVisitor& visitor) = 0;
+};
 
-// Inherting std::enable_shared_from_this allows the object to return a shared pointer pointing to itself
+/*
+   Inherting std::enable_shared_from_this allows the object to return a shared pointer pointing to itself
+*/ 
 struct Binary : Expr, std::enable_shared_from_this<Binary>
 {
    const std::shared_ptr<Expr> left;
    const Token op;
    const std::shared_ptr<Expr> right;
 
-   Binary(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right)
-   : left(left), op(op), right(right)
-   {}
+   Binary(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right);
+
+   std::any accept(ExprVisitor &visitor) override;
+};
+
+struct Group : Expr, public std::enable_shared_from_this<Group>
+{
+    const std::shared_ptr<Expr> expr_in;
+
+    explicit Group(std::shared_ptr<Expr> expr);
+
+    std::any accept(ExprVisitor &visitor) override;
+};
+
+struct Literal : Expr, public std::enable_shared_from_this<Literal>
+{
+    const std::any value;
+
+    explicit Literal(std::any value);
+
+    std::any accept(ExprVisitor &visitor) override;
+};
+
+struct Unary : Expr, public std::enable_shared_from_this<Unary>
+{
+    const Token op;
+    const std::shared_ptr<Expr> right;
+
+    Unary(Token op, std::shared_ptr<Expr> right);
+
+    std::any accept(ExprVisitor &visitor) override;
 };
